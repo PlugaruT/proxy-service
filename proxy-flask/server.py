640bb69f00751426c7caee75ad92cd0f4e56453a
Mihai Coslet <mcosletjr@gmail.com>
2017-01-26 14:30:16 +0200

    allow redirect (for get). parse url

--- proxy-flask/server.py
+++ proxy-flask/server.py
@@ -2,16 +2,21 @@
 run "pip install flask", or run "pip install -r requirements/flask.txt"
 '''
 
-from flask import Flask, request
+from flask import Flask, request, Response
+from flask_cors import CORS, cross_origin
+import requests
+#
 app = Flask(__name__)
+CORS(app)
 
+def parse_url(url, request, server_route):
+    args = ''
+    for arg, value in request.args.items():
+        args += '?{}={}'.format(arg, value)
+    return '{}{}{}'.format(server_route, url, args)
 
 @app.route('/<path:url>')
-def hello_world(url):
-    print(request.args)
-    print(request.base_url)
-    print(request.url)
-    print(request.content_type)
-    print(request.headers)
-    print(request.method)
-    return str(url)
+def mega_proxy_mastermind(url):
+    url_to_send = parse_url(url, request, 'http://localhost:8000/')
+    r = requests.get(url_to_send)
+    return Response(r.text, content_type=r.headers['content-type'])
